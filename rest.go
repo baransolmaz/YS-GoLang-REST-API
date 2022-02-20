cefc9194e7883753c8954683495ff63b378de54f
baransolmaz <b.solmaz2018@gtu.edu.tr>
2022-02-20 16:21:19 +0300

    Get EndPoint Added

--- rest.go
+++ rest.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -11,22 +12,69 @@ type Data struct {
 	Key   string "json:\"Key\""
 	Value string "json:\"Value\""
 }
-type Datas map[string]Data
+type Datas map[string]string
 
-func allDatas(w http.ResponseWriter, r *http.Request) {
-	datas := Datas{
-		"Test_Key0": Data{Key: "Test_Key0", Value: "Test_Value0"},
-		"Test_Key1": Data{Key: "Test_Key1", Value: "Test_Value1"},
+func newDatas() *Datas {
+	return &Datas{
+		"Test_Key0": "Test_Value0",
+		"Test_Key1": "Test_Value1",
 	}
-	fmt.Println("EndPoint Hit: All Datas EndPoint")
-	json.NewEncoder(w).Encode(datas)
 }
+func (d Datas) get(w http.ResponseWriter, r *http.Request) {
+	bodybytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	var data Data
 
-func homepage(w http.ResponseWriter, r *http.Request) {
+	err = json.Unmarshal(bodybytes, &data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadGateway)
+		w.Write([]byte("No Key Found"))
+		return
+	}
+	if _, ok := d[data.Key]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Key Not Found\n"))
+		return
+	}
+	fmt.Println(data)
+	fmt.Println("EndPoint Hit: GET EndPoint")
+	json.NewEncoder(w).Encode(d[data.Key])
+
+}
+func (d Datas) put(w http.ResponseWriter, r *http.Request) {
+
+}
+func (d Datas) delete(w http.ResponseWriter, r *http.Request) {
+
+}
+func (d *Datas) datas(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		d.get(w, r)
+		return
+	case "PUT":
+		d.put(w, r)
+		return
+	case "DELETE":
+		d.delete(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method Not Allowed\n"))
+		return
+	}
+}
+func (d Datas) homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage EndPoint Hit")
 }
+
 func handleRequests() {
-	http.HandleFunc("/", homepage)
-	http.HandleFunc("/datas", allDatas)
+	datas := newDatas()
+	http.HandleFunc("/", datas.homepage)
+	http.HandleFunc("/datas", datas.datas)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
