58c5d65ab5c1a0609eed8ce3604822da4c207f84
baransolmaz <b.solmaz2018@gtu.edu.tr>
2022-02-21 22:14:29 +0300

    TestFileRead Added

--- rest_test.go
+++ rest_test.go
@@ -1,7 +1,22 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestFileRead(t *testing.T) {
-
+	d := Datas{}
+	current, _ := os.Getwd()
+	name := []string{(current + "/tmp/test.json")}
+	read := *d.loadJson(name)
+	if len(read) != 3 {
+		t.Errorf("Expected: 3, Size: %d", len(read))
+	}
+	if read["Key"] != "Value" {
+		t.Errorf("Expected: Value, Recieved: %s", read["Key"])
+	}
+	if read["Test_Key0"] != "Test_Value0" {
+		t.Errorf("Expected: Test_Value0, Recieved: %s", read["Test_Key0"])
+	}
 }
